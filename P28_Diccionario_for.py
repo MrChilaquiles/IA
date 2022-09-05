3141edf268f3e045b764c52267c1d22525ba7384
Rogelio García <RGGA2000@gmail.com>
2022-09-05 01:31:06 -0500

    Practica 28

---
+++ P28_Diccionario_for.py
@@ -0,0 +1,14 @@
+teclado1 = {
+	'Categoría': 'Teclados',
+	'Modelo': 'HyperX Alloy FPS Pro',
+	'Precio': '89,99'
+}
+
+teclado2 = {
+	'Categoría': 'Teclados',
+	'Modelo': 'Corsair K55 RGB',
+	'Precio': '59,99'
+}
+
+for x, y in teclado1.items(): #Se implementa la funcion 'items()' que obtiene los elementos del diccionario y atributos del mismo
+	print(x,"=",y,".") #Se imprime el contenido
