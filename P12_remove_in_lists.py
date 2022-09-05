ae195299e25540ef263818ada3955830d93a032c
Rogelio García <RGGA2000@gmail.com>
2022-09-04 23:39:31 -0500

    Practica 12

---
+++ P12_remove_in_lists.py
@@ -0,0 +1,6 @@
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marron', 'lila', 'negro', 'rosa', 'blanco', 'naranja'] #Se declara la lista con los arrays
+
+colores.remove('amarillo') #Se elimina el color amarillo
+colores.remove('rojo') #Se elimina el color rojo
+
+print(colores) #Se comprueban los nuevos valores
