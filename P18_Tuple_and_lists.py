4b68eb194edd812a0719205ee9aa65e1d6796cd7
Rogelio García <RGGA2000@gmail.com>
2022-09-05 00:21:20 -0500

    Practica 18

---
+++ P18_Tuple_and_lists.py
@@ -0,0 +1,4 @@
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja'] #Se crea la lista con su contenido
+
+tupla = tuple(colores) #convertimos lista a tupla
+print(type(tupla)) #Se imprime para erificar la conversion
