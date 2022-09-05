bb92e7b08bc142ce25996f732bc143e4e9d0c4fb
Rogelio García <RGGA2000@gmail.com>
2022-09-04 22:27:13 -0500

    Practica 3

---
+++ P3_ConcatenenarStrings.py
@@ -0,0 +1,13 @@
+str1 = "Hello" #Se declara la string con su contenido
+str2 = 'world'
+str3 = str1 + str2 #Se concatena los dos strings con el operador "+"
+
+print(str3) #Imprimimos el string con su contenido
+str1 = str1 + str2 #Se concatenan los strings reciclando una variable
+
+print(str1) #Imprimimos el string con su contenido
+print("Hello " + str2 + "<3") #Se concatena una variable con otros
+
+
+
+
