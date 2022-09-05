7433a3d7c880ca4661a839beae17e7c8c1fc9a1a
Rogelio García <RGGA2000@gmail.com>
2022-09-05 00:03:35 -0500

    Practica 15

---
+++ P15_Insert_in_lists.py
@@ -0,0 +1,6 @@
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marron', 'lila', 'negro', 'rosa', 'blanco', 'naranja'] #Se crea la lista con su contenido
+
+colores.insert(-4,'magenta') #Se añade un nuevo valor a determinada localizacion dentro de la lista
+colores.insert(-1,'turquesa') #Se añade un nuevo valor a determinada localizacion dentro de la lista
+
+print(colores) #Se muestra la lista con los elementos añadidos
