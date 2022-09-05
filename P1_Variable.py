6da8f9f370c81839c6466aff4bf01f1211d8ea14
MrChilaquiles <106293025+MrChilaquiles@users.noreply.github.com>
2022-09-04 21:35:13 -0500

    Delete P1_Variable.py

--- P1_Variable.py
+++
@@ -1,8 +0,0 @@
-Hello = "Hello world" #Se almacena el string en la variable
-print(Hello) #Se imprime el valor almacenado
-
-n1 = 12 #Se declara la variable con su contenido
-n2 = 32 #Se declara la variable con su contenido
-n = n1+n2 #Se suman los dos valores y se almacena en la variable n
-print(n) #Se imprime el resultado de la suma
-
