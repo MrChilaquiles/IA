45c5bd046364c3f8c9dd81d863786ca59b86cc8f
Rogelio García <RGGA2000@gmail.com>
2022-09-05 01:10:47 -0500

    Practica 23

---
+++ P23_While.py
@@ -0,0 +1,19 @@
+print("Ejecucuion del primer ciclo")
+x = 0
+while x <= 15: #Se ejecuta el primer ciclo mientras que x <= 15
+    print(x)
+    x += 5 #Se incrementa en 5
+print("\n\n")
+
+print("Ejecucion del segundo ciclo")
+x = 0
+while x >= -100: #Se ejecuta el primer ciclo mientras que x >= -100
+    print(x)
+    x -= 20 #Se decrementa en -20
+print("\n\n")
+
+print("Ejecucion del tercer ciclo")
+x = 10
+while x >= 0:
+    print('El valor del bucle es: ' + str(x)) #Se imprime el resultado hasta que x sea 0
+    x -= 1 #Se decrementa en -1
