6d1426a250d605c7a32b49c3ef100629b8f83067
Rogelio García <RGGA2000@gmail.com>
2022-09-04 22:52:27 -0500

    Practica 6

---
+++ P6_Operators.py
@@ -0,0 +1,11 @@
+p87 = 28+23+30+4+2 #Sumamos valores para que nos de como resultado de 87
+print(p87)
+
+pn87 = 20+5+3+23-28-23-87 #Restamos valores para que nos de como resultado 87
+print(pn87)
+
+mul = 20*23*1.8914 #Multiplicamos valores para que nos de como resultado 87
+print(mul)
+
+div = 5000/230/2 #Dividimos valores para que nos de como resultado 10
+print(div)
