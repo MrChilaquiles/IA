9b301da63b39526cdc646563e5c93b2641475ac2
Rogelio García <RGGA2000@gmail.com>
2022-09-04 22:36:42 -0500

    Practica 5

---
+++ P5_Salto_Tab.py
@@ -0,0 +1,3 @@
+print("-Python.\n-JavaScript.\n-Java.\n-PHP.\n-TypeScript.\n-SQL.\n-COBOL.") #Se imprimimen las palabras con el salto de linea \n
+
+print("\t-Python.\n\t-JavaScript.\n\t-Java.\n\t-PHP.\n\t-TypeScript.\n\t-SQL.\n\t-COBOL.") #Se imprimime el salto junto con una tabulación \t
