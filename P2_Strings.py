94d85a18f266ebe82640799bab2a335c6d42313f
Rogelio García <RGGA2000@gmail.com>
2022-09-04 22:16:45 -0500

    Practica 2

---
+++ P2_Strings.py
@@ -0,0 +1,7 @@
+str1= 'String con comillas simples' #Se declara string con comillas sencillas
+str2 = "String con comillas dobles" #Se declara string con comillas dobles
+
+print(str1); print(str2) #Se manda a imprimir el contenido de las strings
+
+p = '"print()" es utilizado para imprimir valores en la consola' #Se almacena el contenido de las comillas dentro de la string
+print(p) #Mandamos a llamar la string pra imprimirla
