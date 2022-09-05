b17994c072583c2a3cc9bc09be7374c4800df71d
Rogelio García <RGGA2000@gmail.com>
2022-09-04 23:23:57 -0500

    Practica 10

---
+++ P10_NegativePositions.py
@@ -0,0 +1,6 @@
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+print(colores[10]) #Imprime el color en la posicion 10
+print(colores[-7]) #Imprime el color en la posicion -7
+print(colores[6]) #Imprime el color en la posicion -6
+print(colores[-2]) #Imprime el color en la posicion -2
+print(colores[1]) #Imprime el color en la posicion 1
