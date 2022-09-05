1d5cc4b7ff895422c26122ecb11e58806af26226
Rogelio García <RGGA2000@gmail.com>
2022-09-04 22:32:51 -0500

    Practica 4

---
+++ P4_title_upper_lower.py
@@ -0,0 +1,9 @@
+n = "garcia aguilar rogelio gael" #Se declara un string con minusculas
+print(nombre.title()) #title hace que se pongan en mayusculas las primeras letras de cada palabra
+p = "Esta Es Una Frase Para Ser Formateada." #declaramos string con mayusculas y minusculas
+
+print(p.upper()) #Se imprime el string en mayusculas
+print(p.lower()) #Se imprime el string en minusculas
+
+
+
