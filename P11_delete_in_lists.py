589ab1119849b1145fedfe9ca13103a61701b58b
Rogelio García <RGGA2000@gmail.com>
2022-09-04 23:35:59 -0500

    Practica 11

---
+++ P11_delete_in_lists.py
@@ -0,0 +1,8 @@
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja'] #Se declara la lista con los arrays
+
+del colores[1] #Se elimina el color azul
+del colores[3] #Se elimina el color marron
+del colores[-4] #Se elimina el color negro
+del colores[-3] #Se elimina el color rosa
+
+print(colores)#Se imprime para comprobar las eliminaciones realizadas
