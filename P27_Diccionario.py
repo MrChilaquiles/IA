e2f00ebff429101e9086cee3dbb51494b9ddf8cc
Rogelio García <RGGA2000@gmail.com>
2022-09-05 01:26:01 -0500

    Practica 27

---
+++ P27_Diccionario.py
@@ -0,0 +1,15 @@
+teclado1 = {
+	'Categoría': 'Teclados',
+	'Modelo': 'HyperX Alloy FPS Pro', #declaracion de teclado 1
+	'Precio': '89,99'
+}
+
+teclado2 = {
+	'Categoría': 'Teclados',
+	'Modelo': 'Corsair K55 RGB',     #declaracion teclado 2
+	'Precio': '59,99'
+}
+
+c1 = teclado2["Modelo"] # Se almacena el valor de modelo en c1
+c2 = teclado2["Precio"] # Se guarda el valor de precio en c2
+print("El modelo " + c1 + " cuesta " + c2 + " $ .")
