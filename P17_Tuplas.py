ee01765fac7ae86a0cc60fac95e2ab4c12183237
Rogelio García <RGGA2000@gmail.com>
2022-09-05 00:17:38 -0500

    Practica 17

---
+++ P17_Tuplas.py
@@ -0,0 +1,5 @@
+colores = ('rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja') #Se crea la lista con su contenido
+print(colores[1]) #Se imprime la segunda posicion de la lista
+
+numeros = (10, 1, 5, 11) #Se crea la lista con su contenido
+print("El resultado es: ", numeros[0] + numeros[3] - numeros[1] + numeros[2]) #Se imprime el resultado
