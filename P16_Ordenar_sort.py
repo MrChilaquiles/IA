c2a5dca3ecfa7b93e3bf665a997572e68ce626e3
Rogelio García <RGGA2000@gmail.com>
2022-09-05 00:08:47 -0500

    Practica 16

---
+++ P16_Ordenar_sort.py
@@ -0,0 +1,5 @@
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja'] #Se crea la lista con su contenido
+
+colores.sort(reverse=True) #Se ordena la lista al reverso alfabeticamente
+
+print(colores) #Se muestra el nuevo orden alfabeticamente
