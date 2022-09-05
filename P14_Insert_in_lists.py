3201df862dee46da6687c9c63ecdced6fb3851b2
Rogelio García <RGGA2000@gmail.com>
2022-09-04 23:57:35 -0500

    Practica 14

---
+++ P14_Insert_in_lists.py
@@ -0,0 +1,6 @@
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marron', 'lila', 'negro', 'rosa', 'blanco', 'naranja'] #Se crea la lista con su contenido
+
+colores.append('fuxia') #Se añade un elemento
+colores.append("celeste") #Se añade un elemento
+
+print(colores) #Se muestra la lista con los elementos añadidos
