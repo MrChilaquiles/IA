dd27808fc5adb7f7959f4187d7a881a76b0170cd
Rogelio García <RGGA2000@gmail.com>
2022-09-05 00:42:10 -0500

    Practica 19

---
+++ P19_Condicional_If.py
@@ -0,0 +1,17 @@
+num1 = 15
+num2 = 20
+
+if num1 <= num2: #Si num1 es menor o igual a num2 se ejecuta
+	print('Se ejecuta el if.')
+
+num1 = 1450
+num2 = 60
+
+if num1 > num2:
+	print('Se ejecuta el if.')
+
+num1 = 60
+num2 = 60
+
+if num1 != num2:
+	print('Se ejecuta el if.')
